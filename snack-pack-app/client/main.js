8e4a1003d754f5ca1e7dbc28512768fe2264c59c
stephen <davi1138@purdue.edu>
2018-09-23 11:43:14 -0400

    removed meteor framework

--- snack-pack-app/client/main.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { render } from 'react-dom';
-import App from '../imports/ui/App.js';
-
-Meteor.startup(() => {
-	render(<App />, document.getElementById('app'));
-});
-
--- snack-pack-app/imports/ui/README.txt
+++
@@ -1,5 +0,0 @@
-README
-
-this folder is for ui components, each component should be a js file with the name of
-the file being the name of the component.
-
