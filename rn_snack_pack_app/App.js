c8b2fc325813343c774c0dc5cddb4a37942a6bb8
PHSC138 <PHSC138@users.noreply.github.com>
2018-09-27 01:18:31 -0400

    add AWS support to package-lock & imported to app.js

--- rn_snack_pack_app/App.js
+++ rn_snack_pack_app/App.js
@@ -10,6 +10,14 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import SnackPacks from "./src/snackpacks";
 
+//ref: https://docs.aws.amazon.com/aws-mobile/latest/developerguide/mobile-hub-react-native-getting-started.html#mobile-hub-react-native-getting-started-configure-aws-amplify
+import Amplify, {API, Analytics} from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native';
+import aws_exports from './src/aws-exports';
+
+//Allow analytics & other aws backend to connect to mobile hub
+Amplify.configure(aws_exports);
+
 export default class App extends Component {
   render() {
     return (
