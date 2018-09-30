021338a93ee26b1f8b9f485a4d8280455edf625e
Ryan Chen <ryanjchen@yahoo.com>
2018-09-30 13:48:06 -0400

    Added documentation and created SnackPackstoJSON method

--- snackpacks-server/src/snackConnect.js
+++ snackpacks-server/src/snackConnect.js
@@ -17,37 +17,35 @@ class snackConnector{
 
 	//get method to return array of snackPacks
 	getSnackPacks(callback){
+		//callback to initiate connection to AWS RDS
 		var connection = mysql.createConnection({host:this.host, user:this.user, password:this.password, port:this.port});
 		connection.connect(function(err) {
 			if (err) throw err;
+			//callback to send query
 			connection.query("SELECT * FROM snackpacks.snackpacks", function(err, result, fields){
 				if (err) throw err;
+				//callback to end connection
 				connection.end(function(err) {
 					if (err) {
 						return console.log('error:' + err.message);
 					}
-					// console.log("Accessing!");
 					var count = 0;
 					var list_snackpacks=[];
 					for(var r in result){
 						var pack = result[r];
 						console.log(list_snackpacks);
-						list_snackpacks.push(new SnackPack(count, pack.name, pack.contents, pack.allergens, pack.image_path, pack.reviews, pack.cost));
+						list_snackpacks.push(new SnackPack(pack.idsnackpacks, pack.name, pack.contents, pack.allergens, pack.image_path, pack.reviews, pack.cost));
 						count++;
 					}
-					// console.log(list_snackpacks);
-					// console.log("Done!");
-					// console.log('Close the database connection.');
 					callback(null, list_snackpacks);
 				});
 			});
 		});
-
-		// return "list_snackpacks";
 	}
 
 	//createSnackPack
-	//returns true if successful, otherwise returns false 
+	//returns true if successful, otherwise returns false
+	//todo add check
 	createSnackPack(name, contents, allergens, image_path, reviews, cost, callback){
 		var connection = mysql.createConnection({host:this.host, user:this.user, password:this.password, port:this.port});
 		connection.connect(function(err){
@@ -56,59 +54,21 @@ class snackConnector{
 			//Get new id number by using count
 			connection.query("SELECT COUNT(*) FROM snackpacks.snackpacks", function(err, count_result, fields){
 				if(err) throw err;
-				//Structure query then submit
-				connection.query(("INSERT INTO snackpacks.snackpacks VALUES (" + count_result[0]['COUNT(*)'] + ",\"" + name + "\",\"" + contents + "\",\"" + allergens + "\",\"" + image_path + "\",\"" + reviews + "\"," + cost + ")"), function(err, result, fields){
-					connection.end(function (err){
-						if(err){
-							throw err;
-						}
-						console.log(count_result[0]['COUNT(*)']);
-						var ret = true;
-						callback(null, ret);
+				connection.query(`SELECT * FROM snackpacks.users WHERE name=${name}`, function(err, found_result, fields){;
+					//Structure query then submit
+					connection.query(("INSERT INTO snackpacks.snackpacks VALUES (" + count_result[0]['COUNT(*)'] + ",\"" + name + "\",\"" + contents + "\",\"" + allergens + "\",\"" + image_path + "\",\"" + reviews + "\"," + cost + ")"), function(err, result, fields){
+						connection.end(function (err){
+							if(found_result){
+								callback(null, false);
+							}else{
+								console.log(count_result[0]['COUNT(*)']);
+								callback(null, true);
+							}
+						});
 					});
 				});
 			});
 		});
-	}
-
-	//Method to create a user in the database
-	//Returns a SnackUser datatype if success, returns false if error at any point
-	createUser(name, password, address, rewardsPoints, paymentInfo, callback){
-		var connection = mysql.createConnection({host:this.host, user:this.user, password:this.password, port:this.port});
-		var id_num;
-		connection.connect(function(err) {
-			if (err) throw err;
-			console.log("Connected!");
-			connection.query("SELECT COUNT(*) FROM snackpacks.users", function(err, count_result, fields){
-				if(err) throw err;
-				connection.query(("INSERT INTO snackpacks.users VALUES (" + count_result[0]['COUNT(*)'] + "," + name + "," + address + ", null, 0, \"payment\")"), function(err, result, fields){
-					if (err) throw err;
-					console.log("success");
-					var ret = new SnackUser(id_num, name, address, null, 0, "payment");
-					callback(null, ret);
-				});
-			});
-		});
-
-		// return SnackUser(id_num, name, address, null, 0, "payment");
-	}
-
-	//Accessor method to get a certain user by name and password
-	//returns a SnackUser object
-	//TODO: integrate more secure authentication
-	getUser(name, password){
-		var connection = mysql.createConnection({host:this.host, user:this.user, password:this.password, port:this.port});
-		var ret;
-		connection.connect(function(err) {
-			if (err) throw err;
-			console.log("Connected!");
-			connection.query(("SELECT * FROM snackpacks.users where name = " + name + " password = " + password), function(err, result, fields) {
-				if (err) throw err;
-				ret = result[0];
-				console.log("success");
-			});
-		});
-		return SnackUser(ret.iduser, ret.name, ret.addresses, ret.prevOrders, ret.rewardsPoints, ret.paymentInfo);
 	}
 }
 //Allows module to be exposed
--- snackpacks-server/src/snackpack.js
+++ snackpacks-server/src/snackpack.js
@@ -1,32 +1,46 @@
 //snackpack.js
-//ES5
+//ES6
 //Purpose: define the SnackPack data type
 
 //Constructor
 
 
 //Allows module to be exposed
-module.exports = class SnackPack{
+class SnackPack{
 	constructor(id, name, contents, allergens, image_path, reviews, cost){
-		// console.log("New Snackpack");
 		this.id = id;
 		this.name = name;
-		this.contents = contents;
-		this.allergens = allergens;
+		this.contents = contents.split(",");
+		this.allergens = allergens.split(",");
 		this.image_path = image_path;
 		this.reviews = reviews;
 		this.cost = cost;
 	}
 
-	getID(){
+	//Return SnackPack object as JSON
+	get json(){
+		return JSON.stringify({id: this.id, name: this.name, contents: this.contents, allergens: this.allergens, image_path: this.image_path, reviews: this.reviews, cost: this.cost});
+	}
+
+	//Return ID of SnackPack as integer
+	get ID(){
 		return this.id;
 	}
 
-	getName(){
+	//Return name of SnackPack as String
+	get name(){
 		return this.name;
 	}
 
-	getContents(){
+	//Return allergens variable as Javascript list
+	get contents(){
 		return this.contents;
 	}
+
+	//Return allergens variable as Javascript list
+	get allergens(){
+		return allegens;
+	}
 };
+
+module.exports = SnackPack;
