7d9a1a634e7a945628b7e3941fcf00e985d702c4
Stephen <davi1138@purdue.edu>
2018-10-04 11:20:14 -0400

    Added Navigation bar to the bottom of the UI

--- rn_snack_pack_app/src/components/SnackPackMenuView.js
+++ rn_snack_pack_app/src/components/SnackPackMenuView.js
@@ -44,11 +44,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 4,
-        width: '90%'
+        width: '90%',
     },
 
     flatlist_style: {
-        height: '50%'
+        height: '45%'
     },
 
     title_style: {
