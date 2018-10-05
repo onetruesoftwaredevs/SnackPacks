7ef036a3a58a2286c95ac1e11c8675cf9751bf9e
Stephen <davi1138@purdue.edu>
2018-10-05 05:33:53 -0400

    Implemented cart functionality

--- rn_snack_pack_app/src/components/CartView.js
+++ rn_snack_pack_app/src/components/CartView.js
@@ -8,36 +8,22 @@ import React, {Component} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import OrderItemView from "./OrderItemView";
 import PaymentView from "./PaymentView";
+import Cart from '../function/Cart'
 
 export default class CartView extends Component {
-    price; // the value of the price to be displayed
-
-    // TODO change this method to populate with actual data
     render() {
+        let cartData = Cart.getInstance().getItemsInCart();
+        let cartSubtotal = Number(Cart.getInstance().total_cost).toFixed(2);
+
         return (
             <View style={styles.container}>
                 <Text style={styles.title_style}>My Cart</Text>
                 <FlatList
                     style={styles.flatlist_style}
-                    data = {[
-                        {key: 'sp1', spprice: 5.99, spquantity: 2},
-                        {key: 'sp2', spprice: 6.99, spquantity: 3},
-                        {key: 'sp3', spprice: 7.99, spquantity: 4},
-                        {key: 'sp4', spprice: 8.99, spquantity: 5},
-                        {key: 'sp5', spprice: 9.99, spquantity: 7},
-                        {key: 'sp6', spprice: 10.99, spquantity: 9},
-                        {key: 'sp7', spprice: 2.99, spquantity: 1},
-                        {key: 'sp1', spprice: 5.99, spquantity: 2},
-                        {key: 'sp2', spprice: 6.99, spquantity: 3},
-                        {key: 'sp3', spprice: 7.99, spquantity: 4},
-                        {key: 'sp4', spprice: 8.99, spquantity: 5},
-                        {key: 'sp5', spprice: 9.99, spquantity: 7},
-                        {key: 'sp6', spprice: 10.99, spquantity: 9},
-                        {key: 'sp7', spprice: 2.99, spquantity: 1},
-                    ]}
-                    renderItem={({item}) => <OrderItemView spname={item.key} spprice={item.spprice} spquantity={item.spquantity}/>}
+                    data={cartData}
+                    renderItem={({item}) => <OrderItemView spname={item.spname} spprice={item.spprice} spquantity={item.spquantity}/>}
                 />
-                <PaymentView subtotal={50.00} tax={7.89} deliveryFee={6.99}/>
+                <PaymentView subtotal={cartSubtotal} tax={7.89} deliveryFee={6.99}/>
             </View>
         );
     }
@@ -48,7 +34,6 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 4,
         width: '90%',
-        //backgroundColor: '#008844'
     },
 
     flatlist_style: {
