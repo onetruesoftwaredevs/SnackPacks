ec172e494ae2a8b07f1c21e9cf75a4db2ddefbb5
Stephen <davi1138@purdue.edu>
2018-10-04 22:43:25 -0400

    Made Cart.js a singleton

--- rn_snack_pack_app/src/function/Cart.js
+++ rn_snack_pack_app/src/function/Cart.js
@@ -1,76 +1,84 @@
-
-
 var SnackPack = require('./SnackPack');
 
-class Cart{
-	
-	constructor(){
-		this.url = 'https://hz08tdry07.execute-api.us-east-2.amazonaws.com/prod/test';
-		this.Cart = new Array();
-	}
-	
-	function Item(SnackPack, quantity) {
-		this.SnackPack = SnackPack;
-		this.quantity = quantity;
-	}
+class Cart {
 
-	addToCart(SnackPack){
-		Cart.push(new Item(SnackPack, 1));
-	}
-	
-	removeFromCart(SnackPack){
-		for (var i=0; i < Cart.length; i++) {
-			if (Cart[i].SnackPack === SnackPack) {
-				Cart.splice(i, 1);
-			}
-		}
-	}
-	
-	changeQuantity(SnackPack, quantity){
-		for (var i=0; i < Cart.length; i++) {
-			if (Cart[i].SnackPack === SnackPack) {
-				Cart[i].quantity = quantity;
-				return;
-			}
-		}
-	}
-	
-	getSnackPacksInCart(){
-		var SnackPacks = new Array();
-		for (var i=0; i < Cart.length; i++) {
-			SnackPacks.push(Cart[i].SnackPack);
-		}
-		return SnackPacks;
-	}
-	
-	getSnackPack(indexInCart){
-		return Cart[indexInCart].SnackPack;
-	}
-	
-	getQuantity(indexInCart){
-		return Cart[indexInCart].quantity;
-	}
-	
-	checkout(){
-		// Upload Cart as a POST request to a different link
-		// Format: an array of ID, quantity pairs
-		var IDs = new Array(Cart.length);
-		var quantities = new Array(Cart.length);
-		for (var i=0; i < Cart.length; i++) {
-			IDs.push(getSnackPack(i).id);
-			quantities.push(getQuantity(i));
-		}
-		var cartData = [IDs, quantities];
-		fetch(this.url, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({"IDsAndQuantities":cartData}),
-		});
-	}
-    
+    constructor() {
+        this.url = 'https://hz08tdry07.execute-api.us-east-2.amazonaws.com/prod/test';
+        this.cart = new Array();
+        this.instance = null;
+    }
+
+    getInstance() {
+        if (instance === null) {
+            let instance = new Cart();
+        }
+
+        return instance;
+    }
+
+    function
+    Item(SnackPack, quantity) {
+        this.SnackPack = SnackPack;
+        this.quantity = quantity;
+    }
+
+    addTocart(SnackPack) {
+        cart.push(new Item(SnackPack, 1));
+    }
+
+    removeFromcart(SnackPack) {
+        for (var i = 0; i < cart.length; i++) {
+            if (cart[i].SnackPack === SnackPack) {
+                cart.splice(i, 1);
+            }
+        }
+    }
+
+    changeQuantity(SnackPack, quantity) {
+        for (var i = 0; i < cart.length; i++) {
+            if (cart[i].SnackPack === SnackPack) {
+                cart[i].quantity = quantity;
+                return;
+            }
+        }
+    }
+
+    getSnackPacksIncart() {
+        var SnackPacks = new Array();
+        for (var i = 0; i < cart.length; i++) {
+            SnackPacks.push(cart[i].SnackPack);
+        }
+        return SnackPacks;
+    }
+
+    getSnackPack(indexIncart) {
+        return cart[indexIncart].SnackPack;
+    }
+
+    getQuantity(indexIncart) {
+        return cart[indexIncart].quantity;
+    }
+
+    checkout() {
+        // Upload cart as a POST request to a different link
+        // Format: an array of ID, quantity pairs
+        var IDs = new Array(cart.length);
+        var quantities = new Array(cart.length);
+        for (var i = 0; i < cart.length; i++) {
+            IDs.push(getSnackPack(i).id);
+            quantities.push(getQuantity(i));
+        }
+        var cartData = [IDs, quantities];
+        fetch(this.url, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"IDsAndQuantities": cartData}),
+        });
+    }
+
 };
 
 module.exports = Cart;
