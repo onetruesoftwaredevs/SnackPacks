1901678c1d663c5937b61f34801565371b9b5d84
Ryan Chen <ryanjchen@yahoo.com>
2018-10-05 09:04:36 -0400

    Created getCartCost method

--- snackpacks-server/src/snackConnect.js
+++ snackpacks-server/src/snackConnect.js
@@ -6,6 +6,7 @@
 var SnackPack = require('./snackpack');
 var SnackUser = require('./snackUser');
 var mysql = require('mysql');
+var total_cost;
 class snackConnector{
 	//snackConnector constructor
 	constructor(callback){
@@ -93,6 +94,58 @@ class snackConnector{
 							}
 						});
 					});
+				});
+			});
+		});
+	}
+
+	getSnacks(callback){
+		//callback to initiate connection to AWS RDS
+		var connection = mysql.createConnection({host:this.host, user:this.user, password:this.password, port:this.port});
+		connection.connect(function(err) {
+			if (err) throw err;
+			//callback to send query
+			connection.query("SELECT * FROM snackpacks.snackpacks", function(err, result, fields){
+				if (err) throw err;
+				//callback to end connection
+				connection.end(function(err) {
+					if (err) {
+						return console.log('error:' + err.message);
+					}
+					var count = 0;
+					var list_snackpacks=[];
+					for(var r in result){
+						var pack = result[r];
+						// console.log(list_snackpacks);
+						list_snackpacks.push(new SnackPack(pack.idsnackpacks, pack.name, pack.contents, pack.allergens, pack.image_path, pack.reviews, pack.cost));
+						count++;
+					}
+					callback(null, list_snackpacks);
+				});
+			});
+		});
+	}
+
+	getCartCost(cart, callback){
+		var connection = mysql.createConnection({host:this.host, user:this.user, password:this.password, port:this.port});
+		var cartString = "(";
+		for(var x in cart){
+			cartString += `\"${cart[x]}\",`;
+		}
+		cartString = cartString.substr(0, cartString.length-1);
+		cartString += ")";
+		console.log(cartString);
+
+		connection.connect(function(err) {
+			if (err) throw err;
+			//callback to send query
+			connection.query(`SELECT SUM(cost) FROM snackpacks.snackpacks WHERE idsnackpacks IN ${cartString}`, function(err, result, fields){
+				if (err) throw err;
+				//callback to end connection
+				connection.end(function(err) {
+					total_cost = result[0]['SUM(cost)'];
+					console.log(total_cost);
+					callback(null, total_cost);
 				});
 			});
 		});
