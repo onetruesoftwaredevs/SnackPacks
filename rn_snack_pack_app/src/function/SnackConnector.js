6bb2d9c792bfdb04ce5155dfb4ca318c456905e8
claytondetke <cjdetke@yahoo.com>
2018-10-04 23:40:57 -0400

    Update to no longer use async fetch

--- rn_snack_pack_app/src/function/SnackConnector.js
+++ rn_snack_pack_app/src/function/SnackConnector.js
@@ -9,8 +9,7 @@ class SnackConnector{
 
 	static getSnackPacks(){
 		var url = "https://hz08tdry07.execute-api.us-east-2.amazonaws.com/prod/snackpacks";
-		var jsonData = SnackConnector.fetchAndLog();
-		var jsonSnackPacks = jsonData.body;
+		var jsonSnackPacks = fetch(url);
 		var SnackPacks = new Array();
 		if (jsonSnackPacks === undefined) { return []; }
 		for(var i=0; i<jsonSnackPacks.length; i++){
@@ -25,15 +24,6 @@ class SnackConnector{
 			formattedSPs.push({key: SnackPacks[i].id, spprice: SnackPacks[i].cost, sprating: SnackPacks[i].rating, spallergylist: formattedAllergens});
 		}
 		return formattedSPs;	
-	}
-	
-	const
-    static fetchAndLog = async () => {
-        var url = "https://hz08tdry07.execute-api.us-east-2.amazonaws.com/prod/snackpacks";
-		const response = await fetch(url);
-		const json = await response.json();
-		var jsonData = JSON.parse(json);
-		return jsonData;
 	}
 }
 //Allows module to be exposed
