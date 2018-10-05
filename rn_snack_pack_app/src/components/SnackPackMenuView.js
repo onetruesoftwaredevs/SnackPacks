da4c0e0a92979e710f8cfa7143d6c3ec91df8161
Stephen <davi1138@purdue.edu>
2018-10-05 03:26:39 -0400

    Optimized app load time

--- rn_snack_pack_app/src/components/SnackPackMenuView.js
+++ rn_snack_pack_app/src/components/SnackPackMenuView.js
@@ -9,40 +9,22 @@ import {FlatList, StyleSheet, Text, View} from 'react-native';
 import SnackPackView from "./SnackPackView";
 
 export default class SnackPackMenuView extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            dataSource: []
-        };
-    }
-
-    componentDidMount() {
-        return fetch("https://hz08tdry07.execute-api.us-east-2.amazonaws.com/prod/snackpacks?command=list", {method: 'GET'})
-            .then(response => response.json())
-            .then(responseJson => {
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson
-                });
-            });
-    }
+    spdata; // the snack-pack data json object
 
     render() {
-        if (this.state.isLoading) { return (<View/>); }
         return (
             <View style={styles.container}>
                 <Text style={styles.title_style}>Snack Packs</Text>
                 <FlatList
                     style={styles.flatlist_style}
-                    data={this.state.dataSource}
+                    data={this.props.spdata}
                     renderItem={({item}) => <SnackPackView
                         spname={item._name}
                         spprice={item._cost}
                         sprating={3}
                         spallergylist={item._allergens}
                     />}
+                    keyExtractor={(item, index) => index.toString()}
                 />
             </View>
         );
