d5fab5056a80d52e8b8be70c8c6aa273496a77ae
PHSC138 <PHSC138@localhost.localdomain>
2018-10-08 18:24:34 -0400

    start with skeleton for backend

---
+++ lambda/checkout.js
@@ -0,0 +1,51 @@
+/*'use strict';
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+*/
+
+
+var express = require('express');
+var router = express.Router();
+var braintree = require('braintree');
+
+router.post('/', function(req, res, next) {
+  var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'nb4sdg3y59thysfq',
+    publicKey: 'wx8b5t8rhxzcscmn',
+    privateKey: '0c93596ee360a2bcc82b86ccce98d706'
+  });
+
+  // Use the payment method nonce here
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  var newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
