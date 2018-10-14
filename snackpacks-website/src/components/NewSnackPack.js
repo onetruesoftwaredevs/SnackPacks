cb948b74c775eb97ed4d13a4bf86cfa2f4469c4d
Clayton <cjdetke@yahoo.com>
2018-10-14 15:03:22 -0400

    Added skeleton class for making new snack packs

---
+++ snackpacks-website/src/components/NewSnackPack.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { FormGroup, FormControl} from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import "./NewSnackPack.css";
+
+export default class NewSnackPack extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: null,
+            content: ""
+        };
+    }
+
+    validateForm() {
+        return this.state.content.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        this.setState({ isLoading: true });
+    }
+
+    render() {
+        return (
+            <div className="NewSnackPack">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="content">
+                        <FormControl
+                            onChange={this.handleChange}
+                            value={this.state.content}
+                            componentClass="textarea"
+                        />
+                    </FormGroup>
+                    <LoaderButton
+                        block
+                        bsStyle="primary"
+                        bsSize="large"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                        isLoading={this.state.isLoading}
+                        text="Create"
+                        loadingText="Creating…"
+                    />
+                </form>
+            </div>
+        );
+    }
+}
