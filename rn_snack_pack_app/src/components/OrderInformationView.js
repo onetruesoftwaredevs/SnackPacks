bdd6adcb73ad29a8ab2f02d8688853eadb9b09be
Stephen <davi1138@purdue.edu>
2018-10-16 16:40:52 -0400

    Setup driver view and order ui components

---
+++ rn_snack_pack_app/src/components/OrderInformationView.js
@@ -0,0 +1,102 @@
+/**
+ *
+ * @author Stephen Davis
+ *
+ * @description a view for the order information including ...
+ * - name
+ * - number
+ * - status
+ * - address
+ *
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class OrderInformationView extends Component {
+    name;           // string
+    number;         // number
+    order_status;   // string
+    address;        // string
+
+    render() {
+        // create method or dictionary to dynamically change background color based on status
+        let order_status_style = [styles.order_status, {backgroundColor: '#44AAff'}];
+
+        return (
+            <View style={styles.container}>
+                <View style={styles.horizontal_container}>
+                    <Text style={styles.name_style}>{this.props.name}</Text>
+                    <Text style={styles.number_style}>{this.props.number}</Text>
+                </View>
+                <View style={styles.horizontal_container}>
+                    <Text style={styles.address_style}>{this.props.address}</Text>
+                    <Text style={order_status_style}>{this.props.order_status}</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0,
+        padding: 4,
+        backgroundColor: '#DEDEDE'
+    },
+
+    horizontal_container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+
+    name_style: {
+        color: '#444',
+        fontSize: 16,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'center',
+        textTransform: 'none',
+        padding: 4
+    },
+
+    number_style: {
+        color: '#444',
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'center',
+        textTransform: 'none',
+        padding: 4
+
+    },
+
+    address_style: {
+        color: '#444',
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'center',
+        textTransform: 'none',
+        padding: 4
+
+    },
+
+    status_style: {
+        color: '#fff',
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'center',
+        textTransform: 'none',
+        padding: 4
+    }
+});
