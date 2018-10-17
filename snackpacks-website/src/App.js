724f035971b8903556ce5ecbbcf87cf4eb413769
Clayton <cjdetke@yahoo.com>
2018-10-17 19:05:56 -0400

    Beginning to fill out basic website with features

--- snackpacks-website/src/App.js
+++ snackpacks-website/src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
+import {Link, withRouter} from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "./Routes";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+    this.props.history.push("/login");
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Navbar fluid collapseOnSelect>
+              <Navbar.Header>
+                  <Navbar.Brand>
+                      <Link to="/">SnackPacks</Link>
+                  </Navbar.Brand>
+                  <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                  <Nav pullRight>
+                      {this.state.isAuthenticated
+                          ? <Fragment>
+                              <LinkContainer to="/newsnackpack">
+                                  <NavItem>New Snack Pack</NavItem>
+                              </LinkContainer>
+                              <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                          </Fragment>
+                          : <Fragment>
+                              <LinkContainer to="/login">
+                                  <NavItem>Login</NavItem>
+                              </LinkContainer>
+                          </Fragment>
+                      }
+                  </Nav>
+              </Navbar.Collapse>
+          </Navbar>
+          <Routes childProps={childProps} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ snackpacks-website/src/containers/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () =>
+    <div className="NotFound">
+        <h3>Error 404, page not found.</h3>
+    </div>;
