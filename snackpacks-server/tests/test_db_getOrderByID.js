50826ebeb83b2aff5ae98874b37a066e95f31d14
Ryan Chen <ryanjchen@yahoo.com>
2018-10-17 21:56:48 -0400

    Changed everything to promises

--- snackpacks-server/tests/test_db_getOrderByID.js
+++ snackpacks-server/tests/test_db_getOrderByID.js
@@ -1,16 +1,11 @@
-
 var SnackPack = require('../src/snackpack');
 var SnackUser = require('../src/snackUser');
 var snackDB = require('../src/snackConnect');
-//TEST 1
-console.log("Test 1");
-console.log("Success\n------------\n")
 
-//TEST 2
+//TEST 
 console.log("Test 2:");
 console.log("Getting all snackpacks.");
 var snackConnector = new snackDB();
-
-snackConnector.getDrivers()
-	.then(data => console.log(data))
-	.catch(error => console.log(error));
+snackConnector.getOrderByID(0)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
