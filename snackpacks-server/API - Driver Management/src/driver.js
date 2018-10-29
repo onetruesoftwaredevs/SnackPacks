24472ce1a838bbe9cc5d515e64ce9d3f3fded913
Ryan Chen <ryanjchen@yahoo.com>
2018-10-29 16:20:48 -0400

    Adding this then starting work on lambda

--- snackpacks-server/API - Driver Management/src/driver.js
+++ snackpacks-server/API - Driver Management/src/driver.js
@@ -1,12 +1,14 @@
 //Driver.js
 
 class Driver {
-    constructor(id, name, carmodel, carmake, trips){
+    constructor(id, name, phone, carmodel, carmake, trips, status){
         this.id = id;
         this.name = name;
+        this.phone = phone;
         this.carmodel = carmodel;
         this.carmake = carmake;
         this.trips = trips;
+        this.status = status;
     }
 
     get id(){ return this._id; }
@@ -14,6 +16,9 @@ class Driver {
 
     get name(){ return this._name; }
     set name(value){ this._name = value; }
+
+    get phone(){ return this._phone; }
+    set phone(value){ this._phone = value; }
 
     get carmodel(){ return this._carmodel; }
     set carmodel(value){ this._carmodel = value; }
@@ -23,6 +28,9 @@ class Driver {
 
     get trips(){ return this._trips; }
     set trips(value){ this._trips = value; }
+
+    get status(){ return this._status; }
+    set status(value){ this._status = value; }
 
     checkout(){
         return this.total;
