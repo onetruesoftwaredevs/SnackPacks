3e2ad635081477c134ab2557913aefb3e39ed9e1
Clayton <cjdetke@yahoo.com>
2018-11-01 14:30:57 -0400

    Creating and deleting drivers now works!!

--- snackpacks-website/src/Routes.js
+++ snackpacks-website/src/Routes.js
@@ -8,14 +8,16 @@ import NewSnackPack from "./containers/NewSnackPack";
 import SnackPacks from "./containers/SnackPacks";
 import DriverList from "./containers/DriverList";
 import NewDriver from "./containers/NewDriver"
+import Drivers from "./containers/Drivers"
 
 export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
         <AppliedRoute path="/snackpack/new" exact component={NewSnackPack} props={childProps} />
+        <AppliedRoute path="/snackpack/:id" exact component={SnackPacks} props={childProps} />
         <AppliedRoute path="/drivers" exact component={DriverList} props={childProps} />
         <AppliedRoute path="/drivers/new" exact component={NewDriver} props={childProps} />
-        <AppliedRoute path="/snackpack/:id" exact component={SnackPacks} props={childProps} />
+        <AppliedRoute path="/drivers/:id" exact component={Drivers} props={childProps} />
         <Route component={NotFound} />
     </Switch>;
