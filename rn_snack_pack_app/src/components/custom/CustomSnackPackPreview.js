2b76d248d3f8d67965ed1496c8059d082d71773c
Stephen <davi1138@purdue.edu>
2018-11-07 20:28:06 -0500

    Began implementing custom snackpack UI's

--- rn_snack_pack_app/src/components/custom/CustomSnackPackPreview.js
+++ rn_snack_pack_app/src/components/custom/CustomSnackPackPreview.js
@@ -5,7 +5,7 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {TouchableOpacity, StyleSheet, View, Text} from 'react-native';
 
 export default class CustomSnackPackPreview extends Component {
     // display
@@ -13,23 +13,68 @@ export default class CustomSnackPackPreview extends Component {
     price;      // number
     // metadata
     id;         // number
+    navigation; // object
     // pass-through
     components; // list (snackpack-components)
 
+    _openDetailedView = () => {
+        this.props.navigation.navigate("CustomSnackPack", {
+            name: this.props.name,
+            price: this.props.price,
+        });
+    };
+
     render() {
+        let price = Number(this.props.price).toFixed(2);
         return (
-            <View style={styles.container}>
-                /*                      */
-                /*name             price*/
-                /*                      */
-            </View>
+            <TouchableOpacity onPress={this._openDetailedView}>
+                <View style={styles.container}>
+                    <View style={styles.horizontal_container}>
+                        <Text style={styles.name_style}>{this.props.name}</Text>
+                        <Text style={styles.price_style}>${price}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 0,
-        width: '100%',
-    }
+        paddingBottom: 32,
+        marginBottom: 6,
+        marginRight: 6,
+        //width: '100%',
+        backgroundColor: '#fff'
+    },
+
+    horizontal_container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    name_style: {
+        color: '#444',
+        fontSize: 20,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'top',
+        textTransform: 'none',
+        padding: 4
+    },
+
+    price_style: {
+        color: '#44aa44',
+        fontSize: 16,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'center',
+        textTransform: 'none',
+        padding: 2
+    },
 });
