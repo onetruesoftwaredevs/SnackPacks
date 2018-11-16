5103a0bc5a626736bdca66ec154f69178c9c2b8c
PHSC138 <PHSC138@users.noreply.github.com>
2018-11-15 22:42:10 -0500

    updated lambda backend for payments

--- snackpacks-server/API - Payment/index.js
+++ snackpacks-server/API - Payment/index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-var express=require('express');
-var braintree=require('braintree');
-// var routes=require('./routes/index'); //Current file
-var router=express.Router();
-var gateway=require('./src/gateway.js');
-
-var TRANSACTION_SUCCESS_STATUSES=[
+let braintree=require('braintree');
+let gateway=require('./src/gateway.js');
+let PaymentConnector=require('./src/paymentConnect');
+let TRANSACTION_SUCCESS_STATUSES=[
     braintree.Transaction.Status.Authorizing,
     braintree.Transaction.Status.Authorized,
     braintree.Transaction.Status.Settled,
@@ -17,9 +14,9 @@ var TRANSACTION_SUCCESS_STATUSES=[
 ];
 
 function formatErrors(errors){
-    var formattedErrors='';
+    let formattedErrors='';
 
-    for(var i in errors){
+    for(let i in errors){
         if(errors.hasOwnProperty(i)){
             formattedErrors+='Error: '+errors[i].code+': '+errors[i].message+'\n';
         }
@@ -28,8 +25,8 @@ function formatErrors(errors){
 }
 
 function createResultObject(transaction){
-    var result;
-    var status=transaction.status;
+    let result;
+    let status=transaction.status;
 
     if(TRANSACTION_SUCCESS_STATUSES.indexOf(status)!== -1){
         result={
@@ -48,88 +45,254 @@ function createResultObject(transaction){
     return result;
 }
 
-//All the router.get calls --> lambda "functions"
+exports.handler=function(event,context,callback){
+    console.log(event);
+    console.log(context);
 
-//API CALL GET to getClientToken
-router.get('/checkouts/new',function(req,res){
+    let queryString=event.queryStringParameters;
+    if(queryString!=null){
+        let command=queryString.command;
+        if(command!=null){
+            if(command.localeCompare("getClientToken")===0){//API CALL GET to getClientToken
+                //Server with customer ID to save cards in "braintree vault"
+                /*gateway.clientToken.generate({
+                    customerId:aCustomerId
+                }).then(function(err,response){
+                    var clientToken=response.clientToken
+                });*/
 
-    //Server with customer ID to save cards in "braintree vault"
-    /*gateway.clientToken.generate({
-        customerId:aCustomerId
-    }).then(function(err,response){
-        var clientToken=response.clientToken
-    });*/
+                gateway.clientToken.generate({}).then(function(res){
+                    //Return client token
+                    let response={
+                        "statusCode":200,
+                        "headers":{},
+                        "body":JSON.stringify(res.clientToken),
+                        "isBase64Encoded":"false"
+                    };
+                    callback(null,response);
+                }).catch((err)=>{
+                    console.log("INTO CLIENT TOKEN CATCH ERROR");
+                    let response={
+                        "statusCode":500,
+                        "headers":{},
+                        "body":JSON.stringify(err),
+                        "isBase64Encoded":"false"
+                    };
+                    callback(null,response);
+                });
+            }else if(command.localeCompare("transaction")===0){//API CALL GET to checkout with ID of transaction ?id=id
+                console.log("In transaction");
+                if(queryString!==null&&queryString!==undefined){
+                    if(queryString.id!==undefined&&
+                        queryString.id!==null&&
+                        queryString.id!==""){
+                        console.log("Received id: "+queryString.id);
+                        let id=queryString.id;
 
-    gateway.clientToken.generate({}).then(function(err,response){
-        //Return client token
-        var clientToken=response.clientToken;
+                        //update this to a db call by transaction id
+                        gateway.transaction.find(id).then(function(transaction){
+                            let result=createResultObject(transaction);
 
-        //TODO: check for error
-        //Is this the proper way to return for the lambda function?
-        res.send(response.clientToken);
-    });
+                            //Return transaction to the client to update the transaction page
+                            let object={transaction,result};
+                            let response={
+                                "statusCode":200,
+                                "headers":{},
+                                "body":JSON.stringify(object),
+                                "isBase64Encoded":"false"
+                            };
+                            callback(null,response);
+                        }).catch((err)=>{
+                            console.log("INTO TRANSACTION CATCH ERROR");
+                            let response={
+                                "statusCode":500,
+                                "headers":{},
+                                "body":JSON.stringify(err),
+                                "isBase64Encoded":"false"
+                            };
+                            callback(null,response);
+                        });
+                    }else{
+                        console.log("INTO NO ID IN QUERY");
+                        let response={
+                            "statusCode":500,
+                            "headers":{},
+                            "body":"No id in query",
+                            "isBase64Encoded":"false"
+                        };
+                        callback(null,response);
+                    }
+                }else{
+                    console.log("INTO NO QUERY STRING ERROR");
+                    let response={
+                        "statusCode":500,
+                        "headers":{},
+                        "body":"No query string",
+                        "isBase64Encoded":"false"
+                    };
+                    callback(null,response);
+                }
+            }else if(command.localeCompare("checkout")===0){//API CALL POST to checkout with payment_nonce
+                let transactionErrors;
 
-    /*Client:
-     * app.get("/client_token", function (req, res) {
-     * gateway.clientToken.generate({}, function (err, response) {
-     *  res.send(response.clientToken);
-     *  });
-     *  });
-     */
+                //var amount=req.body.amount; //TODO: Use db calls to get the cost of the cart values here
+                let amount='10.00';
+                let nonce=null; //Declare nonce variable
+                console.log(event.body);
+                if(event.body!==null&&event.body!==undefined){
+                    nonce=event.body;//Set nonce to body's nonce
+                }
+                if(nonce!=null){
+                    //Use Braintree gateway to complete the transaction
+                    gateway.transaction.sale({
+                        amount:amount,
+                        //paymentMethodNonce: "fake-valid-nonce",//Testing
 
-    // Using callbacks
-    // gateway.clientToken.generate({}).then(function(err,response){
-    //     //Send client token to client. This tries to render a web page, AND send the request
-    //     res.render('checkouts/new',{clientToken:response.clientToken,messages:req.flash('error')});
-    // });
-});
+                        paymentMethodNonce:nonce,//nonce from request
+                        options:{
+                            submitForSettlement:true//Send payment
+                        }
+                    }).then(function(result){
+                        if(result.success||result.transaction){//SUCCESS
+                            let response={
+                                "statusCode":200,
+                                "headers":{},
+                                "body":JSON.stringify(result),
+                                "isBase64Encoded":"false"
+                            };
+                            callback(null,response);
+                        }else{//ERROR
+                            transactionErrors=result.errors.deepErrors();
+                            console.log("INTO CHECKOUT ERROR");
+                            let response={
+                                "statusCode":500,
+                                "headers":{},
+                                "body":JSON.stringify(formatErrors(transactionErrors)),
+                                "isBase64Encoded":"false"
+                            };
+                            callback(null,response);
+                        }
+                    }).catch((err)=>{
+                        console.log("INTO CHECKOUT CATCH ERROR");
+                        let response={
+                            "statusCode":500,
+                            "headers":{},
+                            "body":JSON.stringify(err),
+                            "isBase64Encoded":"false"
+                        };
+                        callback(null,response);
+                    });
+                }else{
+                    console.log("INTO NO NONCE ERROR");
+                    let response={
+                        "statusCode":400,
+                        "headers":{},
+                        "body":event.body,
+                        "isBase64Encoded":"false"
+                    };
+                    callback(null,response);
+                }
+            }else if(command.localeCompare("checkout2")===0){//API CALL POST to checkout with payment_nonce
+                let transactionErrors;
 
-//API CALL GET to checkout with ID of transaction
-router.get('/checkouts/:id').then(function(req,res){
-    var result;
-    var transactionId=req.params.id;
+                let nonce=null; //Declare nonce variable
+                let amount=null; //Declare amount variable
+                console.log("EVENT BODY: "+event.body.toString());
+                if(event.body!=null&&event.body!=undefined){
+                    let post=JSON.stringify(event.body);
+                    console.log("post: "+post);
+                    nonce=event.body.nonce;//Set nonce to body's nonce
+                    console.log("nonce: "+nonce);
 
-    //update this to a db call by transaction id
-    gateway.transaction.find(transactionId).then(function(err,transaction){
-        result=createResultObject(transaction);
+                    let paymentConnector= new PaymentConnector();
+                    amount=paymentConnector.getCartCost(event.body.cart);
+                    //amount=paymentConnector.getCartCost(event.body.cart);
+                    console.log("amount: "+amount);
+                }
 
-        //Return transaction to the client to update the transaction page
-        //res.render('checkouts/show',{transaction:transaction,result:result});
 
-        //Is this the proper way to return for the lambda function?
-        res.send(transaction);
-    });
-});
 
-//API CALL POST to checkout with payment_nonce
-router.post('/checkouts',function(req,res){
-    var transactionErrors;
+                if(nonce!=null&&amount!=null){
+                    //Use Braintree gateway to complete the transaction
+                    gateway.transaction.sale({
+                        amount:amount,
+                        //paymentMethodNonce: "fake-valid-nonce",//Testing
 
-    //var amount=req.body.amount; //TODO: Use db calls to get the cost of the cart values here
-    var amount='10.00';
-    var nonce=req.body.payment_method_nonce;//Receive nonce from client
+                        paymentMethodNonce:nonce,//nonce from request
+                        options:{
+                            submitForSettlement:true//Send payment
+                        }
+                    }).then(function(result){
+                        if(result.success||result.transaction){//SUCCESS
+                            let response={
+                                "statusCode":200,
+                                "headers":{},
+                                "body":JSON.stringify(result),
+                                "isBase64Encoded":"false"
+                            };
+                            callback(null,response);
+                        }else{//ERROR
+                            transactionErrors=result.errors.deepErrors();
+                            console.log("INTO CHECKOUT ERROR");
+                            let response={
+                                "statusCode":500,
+                                "headers":{},
+                                "body":JSON.stringify(formatErrors(transactionErrors)),
+                                "isBase64Encoded":"false"
+                            };
+                            callback(null,response);
+                        }
+                    }).catch((err)=>{
+                        console.log("INTO CHECKOUT CATCH ERROR");
+                        let response={
+                            "statusCode":500,
+                            "headers":{},
+                            "body":JSON.stringify(err),
+                            "isBase64Encoded":"false"
+                        };
+                        callback(null,response);
+                    });
+                }else{
+                    console.log("INTO NO NONCE ERROR");
+                    let response={
+                        "statusCode":400,
+                        "headers":{},
+                        "body":event.body,
+                        "isBase64Encoded":"false"
+                    };
+                    callback(null,response);
+                }
+            }else if(command.localeCompare("client")===0){
+                //Testing client functionality for transaction
+                gateway.requestPaymentMethod(function(requestPaymentMethodErr,payload){
+                    let response={
+                        "statusCode":200,
+                        "headers":{},
+                        "body":JSON.stringify(payload.nonce),
+                        "isBase64Encoded":"false"
+                    };
+                    callback(null,response);
+                });
+            }else{
+                console.log("INTO INVALID REQUEST TYPE ERROR");
+                let response={
+                    "statusCode":400,
+                    "headers":{},
+                    "body":"Invalid request type",
+                    "isBase64Encoded":"false"
+                };
+                callback(null,response);
+            }
 
-    //Use Braintree gateway to complete the transaction
-    gateway.transaction.sale({
-        amount:amount,
-        paymentMethodNonce:nonce,
-        options:{
-            submitForSettlement:true//Send payment
         }
-    }).then(function(err,result){
-        if(result.success||result.transaction){//SUCCESS
-            //Return some sort of success message
-            res.send(result);
-            //res.redirect('checkouts/'+result.transaction.id);
-        }else{//ERROR
-            transactionErrors=result.errors.deepErrors();
-            //return msg with errors (using the formatErrors();
-            res.status(500).send(formatErrors(transactionErrors));
-            //req.flash('error',{msg:formatErrors(transactionErrors)});
-            //res.redirect('checkouts/new');
-        }
-    });
-});
-
-//Needs index export for lambda?
-module.exports=router;
+    }else{
+        console.log("INTO QUERY STRING IS NULL ERROR");
+        let response={
+            "statusCode":400,
+            "headers":{},
+            "body":"Query string is null",
+            "isBase64Encoded":"false"
+        };
+        callback(null,response);
+    }
+};
