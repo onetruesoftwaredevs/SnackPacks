c4100c044c77ff615eb20a7b0c9885ad9c8e7fec
Ryan <ryanjengchen@gmail.com>
2018-11-22 10:36:59 -0800

    Maybe this change will work

--- snackpacks-server/API - Snackpacks/src/test.js
+++ snackpacks-server/API - Snackpacks/src/test.js
@@ -5,10 +5,14 @@ var snackDB = require('../src/snackConnect');
 //TEST 
 console.log("Test 1:");
 var snackConnector = new snackDB();
-snackConnector.addReview(1, 5, "Donald Trump", "This is the best", 2, 2, "sdf aasdfasdfsa sadf asdf asdfa s")
+/*snackConnector.addReview(1, 5, "Donald Trump", "This is the best", 2, 2, "sdf aasdfasdfsa sadf asdf asdfa s")
 	.then(data => console.log(data))
 	.catch(error => console.log(error));
 
-// snackConnector.getReviewBySnackpackID(1)
-// 	.then(data => console.log(data))
-// 	.catch(error => console.log(error));
+snackConnector.getReviewBySnackpackID(1)
+ 	.then(data => console.log(data))
+ 	.catch(error => console.log(error));
+*/
+snackConnector.getSnackPacks()
+	.then(data => console.log(data[1]))
+ 	.catch(error => console.log(error));
