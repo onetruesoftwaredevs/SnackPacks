0931bf9cd3f94606cc37515ae288679bd0ca7cb1
Ryan Chen <ryanjchen@yahoo.com>
2018-11-25 18:59:23 -0500

    Added README for blacklist and some input scrubbing in there too

--- snackpacks-server/API - Blacklist/src/blackListConnect.js
+++ snackpacks-server/API - Blacklist/src/blackListConnect.js
@@ -39,14 +39,42 @@ class blackListUserConnector{
 		});
 	}
 
-	// reportBlackListedUser(int user_id, string reason_str)
-	// Adds a blacklisted user to the database, default status of case pending
-	reportBlackListedUser(user_id, reason_str){
+	// getBlackListedUsersByID(userID int)
+	// Returns the user specified by the userID as a blackListUser object
+	getBlackListedUserByID(userID){
 		return new Promise((resolve, reject) => {
 			var connection = mysql.createConnection({host:this.host, user:this.user, password: this.password, port: this.port});
 			connection.connect(function(err){
 				if(err) reject(err);
-				connection.query(`insert into snackpacks.blacklist values("${user_id}", "${reason_str}", 0)`, function(err, result, fields){
+				connection.query(`select * from snackpacks.blacklist where userID=${userID}`, function(err, result, fields){
+					if(err) reject(err);
+					connection.end(function(err){
+						if(err) reject(err);
+						if(result.length > 0){
+							let retList = [];
+							for(var case_id in result){
+								let blackListCase = result[case_id];
+								console.log(blackListCase.status);
+								retList.push(new blackListUser(blackListCase.userID, blackListCase.reason, parseInt(blackListCase.status)));
+							}
+							resolve(retList[0]);
+						}else{
+							reject("ERROR: USERID NOT FOUND IN BLACKLIST DATABASE");
+						}
+					});
+				});
+			});
+		});
+	}
+
+	// reportBlackListedUser(int userID, string reasonStr)
+	// Adds a blacklisted user to the database, default status of case pending
+	reportBlackListedUser(userID, reasonStr){
+		return new Promise((resolve, reject) => {
+			var connection = mysql.createConnection({host:this.host, user:this.user, password: this.password, port: this.port});
+			connection.connect(function(err){
+				if(err) reject(err);
+				connection.query(`insert into snackpacks.blacklist values("${userID}", "${reasonStr}", 0)`, function(err, result, fields){
 					if(err) reject(err);
 					connection.end(function(err){
 						if(err) reject(err);
@@ -59,16 +87,19 @@ class blackListUserConnector{
 
 	// setBlackListUserStatus(int status)
 	// Sets the status of the blackListedUser
-	setBlackListUserStatus(user_id, status){
+	setBlackListUserStatus(userID, status){
 		return new Promise((resolve, reject) => {
+			if(isNaN(status) || (status > 2 || status < 0)){
+				reject("ERR: Status not a number or not in range (0 to 2)");
+			}
 			var connection = mysql.createConnection({host:this.host, user:this.user, password: this.password, port: this.port});
 			connection.connect(function(err){
 				if(err) reject(err);
-				connection.query(`select * from snackpacks.blacklist where userID=${user_id}`, function(err, foundUser, fields){
+				connection.query(`select * from snackpacks.blacklist where userID=${userID}`, function(err, foundUser, fields){
 					// console.log(foundUser);
 					if(err) reject(err);
 					if(foundUser.length > 0){
-						connection.query(`update snackpacks.blacklist set status=${status} where userID=${user_id}`, function(err, result, fields){
+						connection.query(`update snackpacks.blacklist set status=${status} where userID=${userID}`, function(err, result, fields){
 							if(err) reject(err);
 							connection.end(function(err){
 								if(err) reject(err);
@@ -86,30 +117,54 @@ class blackListUserConnector{
 		});
 	}
 
-	// checkUserStatus(int user_id)
+	// checkUserStatus(int userID)
 	// Returns the status of a particular userID. 0 for not found in the blackList, 1 for pending, and 2 for banned!
-	checkUserStatus(user_id){
+	checkUserStatus(userID){
 		return new Promise((resolve, reject) => {
 			var connection = mysql.createConnection({host:this.host, user:this.user, password: this.password, port: this.port});
 			connection.connect(function(err){
 				if(err) reject(err);
-				connection.query(`select * from snackpacks.blacklist where userID=${user_id}`, function(err, foundUser, fields){
+				connection.query(`select * from snackpacks.blacklist where userID=${userID}`, function(err, foundUser, fields){
 					// console.log(foundUser);
 					connection.end(function(err){
 						if(err) reject(err);
 						if(foundUser.length > 0){
 							let blackListUser = foundUser[0];
 							if(blackListUser.status == 0){
+								// Pending
 								resolve(1);
-							}else{
+							}else if(blackListUser.status == 1){
+								// Banned
 								resolve(2);
+							}else{
+								// Not banned
+								resolve(3);
 							}
 						}else{
 							connection.end(function(err){
 								if(err) reject(err);
+								// User not found
 								resolve(0);
 							});
 						}
+					});
+				});
+			});
+		});
+	}
+
+	// cleanDatabase()
+	// Gets rid of all the cases with status marked as 3
+	cleanDatabase(){
+		return new Promise((resolve, reject) => {
+			var connection = mysql.createConnection({host:this.host, user:this.user, password: this.password, port: this.port});
+			connection.connect(function(err){
+				if(err) reject(err);
+				connection.query(`delete snackpacks.blacklist where userID=${userID}`, function(err, result, fields){
+					if(err) reject(err);
+					connection.end(function(err){
+						if(err) reject(err);
+						resolve(true);
 					});
 				});
 			});
