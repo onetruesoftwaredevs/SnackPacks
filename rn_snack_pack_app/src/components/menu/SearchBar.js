3efd77bb80f8e8ee2064ce9bfe93d9f84514f935
Stephen <davi1138@purdue.edu>
2018-11-25 17:13:28 -0500

    Implemented sorting snackpacks by review and popularity

--- rn_snack_pack_app/src/components/menu/SearchBar.js
+++ rn_snack_pack_app/src/components/menu/SearchBar.js
@@ -17,7 +17,8 @@ export default class SearchBar extends Component {
         super();
         this.state = {
             showFilters: false,
-            selectedOption: 'name',
+            selectedSearchOption: 'name',
+            selectedSortOption: 'popularity',
             search: 'none'
         };
     }
@@ -38,33 +39,61 @@ export default class SearchBar extends Component {
     };
 
     _selectName = () => {
-        this.setState({selectedOption: 'name'});
+        this.setState({selectedSearchOption: 'name'});
         Menu.getInstance().setSearchFilter('name');
     };
 
     _selectContents = () => {
-        this.setState({selectedOption: 'allergens'});
+        this.setState({selectedSearchOption: 'allergens'});
         Menu.getInstance().setSearchFilter('contents');
     };
 
-    render() {
-        let nameStyle = this.state.selectedOption === 'name' ? styles.selected_option : styles.not_selected_option;
-        let contentsStyle = this.state.selectedOption === 'name' ? styles.not_selected_option : styles.selected_option;
-        let placeholder = this.state.selectedOption === 'name' ? 'search by name ' : 'search by contents ';
+    _selectPopularity = () => {
+        this.setState({selectedSortOption: 'popularity'});
+        Menu.getInstance().setSortFilter('popularity');
+    };
 
-        let filters = this.state.showFilters ? (
-            <View style={global_stylesheet.horizontal_container_loose}>
-                <Text style={global_stylesheet.data_title_style}>Search by</Text>
-                <View style={global_stylesheet.horizontal_container_tight}>
-                    <TouchableOpacity onPress={this._selectName}>
-                        <Text style={nameStyle}>name</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={this._selectContents}>
-                        <Text style={contentsStyle}>contents</Text>
-                    </TouchableOpacity>
+    _selectReviews = () => {
+        this.setState({selectedSortOption: 'reviews'});
+        Menu.getInstance().setSortFilter('reviews');
+    };
+
+    renderFilters() {
+        let nameStyle = this.state.selectedSearchOption === 'name' ? styles.selected_search_option : styles.not_selected_option;
+        let contentsStyle = this.state.selectedSearchOption === 'name' ? styles.not_selected_option : styles.selected_search_option;
+        let popularityStyle = this.state.selectedSortOption === 'popularity' ? styles.selected_sort_option : styles.not_selected_option;
+        let reviewStyle = this.state.selectedSortOption === 'popularity' ? styles.not_selected_option : styles.selected_sort_option;
+
+        return this.state.showFilters ? (
+            <View>
+                <View style={global_stylesheet.horizontal_container_loose}>
+                    <Text style={global_stylesheet.data_title_style}>Search by</Text>
+                    <View style={global_stylesheet.horizontal_container_tight}>
+                        <TouchableOpacity onPress={this._selectName}>
+                            <Text style={nameStyle}>name</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this._selectContents}>
+                            <Text style={contentsStyle}>contents</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={global_stylesheet.horizontal_container_loose}>
+                    <Text style={global_stylesheet.data_title_style}>Sort by</Text>
+                    <View style={global_stylesheet.horizontal_container_tight}>
+                        <TouchableOpacity onPress={this._selectPopularity}>
+                            <Text style={popularityStyle}>popularity</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this._selectReviews}>
+                            <Text style={reviewStyle}>reviews</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         ) : (<View/>);
+    }
+
+    render() {
+        let placeholder = this.state.selectedSearchOption === 'name' ? 'search by name ' : 'search by contents ';
 
         return (
             <View style={global_stylesheet.basic_container}>
@@ -81,16 +110,29 @@ export default class SearchBar extends Component {
                     </TouchableOpacity>
                 </View>
 
-                {filters}
+                {this.renderFilters()}
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    selected_option: {
+    selected_search_option: {
         color: '#fff',
         backgroundColor: '#4AF',
+        fontSize: 16,
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        textDecorationLine: 'none',
+        textAlignVertical: 'center',
+        textTransform: 'none',
+        padding: 8
+    },
+
+    selected_sort_option: {
+        color: '#fff',
+        backgroundColor: '#4AA',
         fontSize: 16,
         fontStyle: 'normal',
         fontWeight: 'bold',
--- rn_snack_pack_app/src/function/Menu.js
+++ rn_snack_pack_app/src/function/Menu.js
@@ -11,6 +11,7 @@ class Menu {
             this._menu = [];
             this._names = [];
             this._search_filter = "name";
+            this._sort_filter = "popularity";
             this._search = 'none';
             Menu.instance = this;
         }
@@ -44,8 +45,58 @@ class Menu {
         this._search = search;
     }
 
+    setSortFilter(filter) {
+        this._sort_filter = filter;
+    }
+
     getSearchTerm() {
         return this._search;
+    }
+
+    static getAverageRating(item) {
+        let rating = 0;
+        for (let i = 0; i < item.reviews.length; i++) {
+            rating += item.reviews[i].rating;
+        }
+        return rating / item.reviews.length;
+    }
+
+    sortByPopularity() {
+        for (let i = 0; i < this._menu.length - 1; i++) {
+            for (let j = i + 1; j < this._menu.length; j++) {
+                let item_i = this._menu[i];
+                let item_j = this._menu[j];
+                let review_i = Menu.getAverageRating(item_i);
+                let review_j = Menu.getAverageRating(item_j);
+                if (review_i < review_j) {
+                    this._menu[i] = item_j;
+                    this._menu[j] = item_i;
+                }
+            }
+        }
+    }
+
+    sortByReview() {
+        for (let i = 0; i < this._menu.length - 1; i++) {
+            for (let j = i + 1; j < this._menu.length; j++) {
+                let item_i = this._menu[i];
+                let item_j = this._menu[j];
+                let review_i = item_i.reviews.length;
+                let review_j = item_j.reviews.length;
+                if (review_i < review_j) {
+                    this._menu[i] = item_j;
+                    this._menu[j] = item_i;
+                }
+            }
+        }
+    }
+
+    sort() {
+        if (this._sort_filter === 'popularity') {
+            this.sortByPopularity();
+        } else {
+            this.sortByReview();
+        }
     }
 
     getSearch() {
@@ -83,6 +134,8 @@ class Menu {
     }
 
     getData() {
+        this.sort();
+
         if (this._search === 'none') {
             return this._menu;
         }
