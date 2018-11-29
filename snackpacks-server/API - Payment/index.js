a4da457946113c53739f05af6d7fb83a1b5fb7e3
PHSC138 <PHSC138@users.noreply.github.com>
2018-11-29 00:37:09 -0500

    added cash order started integrating submit order to db

--- snackpacks-server/API - Payment/index.js
+++ snackpacks-server/API - Payment/index.js
@@ -3,6 +3,7 @@
 let braintree=require('braintree');
 let gateway=require('./src/gateway.js');
 let PaymentConnector=require('./src/paymentConnect');
+let OrderConnector=require('./src/orderConnect');
 let TRANSACTION_SUCCESS_STATUSES=[
     braintree.Transaction.Status.Authorizing,
     braintree.Transaction.Status.Authorized,
@@ -133,71 +134,14 @@ exports.handler=function(event,context,callback){
                     callback(null,response);
                 }
             }else if(command.localeCompare("checkout")===0){//API CALL POST to checkout with payment_nonce
-                let transactionErrors;
-
-                //var amount=req.body.amount; //TODO: Use db calls to get the cost of the cart values here
-                let amount='10.00';
-                let nonce=null; //Declare nonce variable
-                console.log(event.body);
-                if(event.body!==null&&event.body!==undefined){
-                    nonce=event.body;//Set nonce to body's nonce
-                }
-                if(nonce!=null){
-                    //Use Braintree gateway to complete the transaction
-                    gateway.transaction.sale({
-                        amount:amount,
-                        //paymentMethodNonce: "fake-valid-nonce",//Testing
-
-                        paymentMethodNonce:nonce,//nonce from request
-                        options:{
-                            submitForSettlement:true//Send payment
-                        }
-                    }).then(function(result){
-                        if(result.success||result.transaction){//SUCCESS
-                            let response={
-                                "statusCode":200,
-                                "headers":{},
-                                "body":JSON.stringify(result),
-                                "isBase64Encoded":"false"
-                            };
-                            callback(null,response);
-                        }else{//ERROR
-                            transactionErrors=result.errors.deepErrors();
-                            console.log("INTO CHECKOUT ERROR");
-                            let response={
-                                "statusCode":500,
-                                "headers":{},
-                                "body":JSON.stringify(formatErrors(transactionErrors)),
-                                "isBase64Encoded":"false"
-                            };
-                            callback(null,response);
-                        }
-                    }).catch((err)=>{
-                        console.log("INTO CHECKOUT CATCH ERROR");
-                        let response={
-                            "statusCode":500,
-                            "headers":{},
-                            "body":JSON.stringify(err),
-                            "isBase64Encoded":"false"
-                        };
-                        callback(null,response);
-                    });
-                }else{
-                    console.log("INTO NO NONCE ERROR");
-                    let response={
-                        "statusCode":400,
-                        "headers":{},
-                        "body":event.body,
-                        "isBase64Encoded":"false"
-                    };
-                    callback(null,response);
-                }
-            }else if(command.localeCompare("checkout2")===0){//API CALL POST to checkout with payment_nonce
                 //{"nonce":"nonce","tip":0,"cart":[{"key":0,"quantity":6},{"key":3,"quantity":10}]}
                 let transactionErrors;
-                let tip=null; //Declare tip variable
-                let nonce=null; //Declare nonce variable
-                let amount=null; //Declare amount variable
+                //Declare variables (initialized to undefined)
+                let tip,nonce,cart,amount,street,city,state,zip;
+
+                //let tip=null; //Declare tip variable
+                //let nonce=null; //Declare nonce variable
+                //let amount=null; //Declare amount variable
                 let serviceFee=1; //Declare serviceFee variable
                 //event.body="{\"nonce\":\"tokencc_bc_9k6r9p_dcg5w3_2sfgn8_p5vgj7_52z\",\"tip\":0,\"cart\":[{\"key\":0,\"quantity\":1}]}";
                 console.log("EVENT BODY: "+JSON.stringify(event.body));
@@ -212,17 +156,32 @@ exports.handler=function(event,context,callback){
                     nonce=event.body.nonce;//Set nonce to body's nonce
                     console.log("nonce: "+nonce);
 
+                    cart=event.body.cart;
+                    if(cart===undefined){
+                        console.log("INTO NO CART ERROR");
+                        let response={
+                            "statusCode":400,
+                            "headers":{},
+                            "body":event.body,
+                            "isBase64Encoded":"false"
+                        };
+                        callback(null,response);
+                    }
+
+                    //Check for int & absolute value of tip
+                    console.log(parseFloat(event.body.tip,10));
+                    //Get value of tip in float
+                    if(event.body.tip==parseFloat(event.body.tip,10)&&event.body.tip!=null&&event.body.tip!==undefined) tip=Math.abs(event.body.tip);
+                    else tip=0;
+
                     let paymentConnector=new PaymentConnector();
-                    paymentConnector.getCartCost(event.body.cart).then(function(data){//Calculate cost of cart
+                    paymentConnector.getCartCost(cart).then(function(data){//Calculate cost of cart
                         amount=Number(data);//Amount
 
-                        if(nonce!=null&&amount!=null){
-                            //Check for int & absolute value of tip
-                            console.log(parseFloat(event.body.tip,10));
-                            if(event.body.tip==parseFloat(event.body.tip,10)&&event.body.tip!=null&&event.body.tip!==undefined) tip=Math.abs(event.body.tip);
-                            else tip=0;
+                        if(nonce!==undefined&&amount!==undefined){
                             let tax=Number(Number(amount*0.06).toFixed(2)); //Calculate tax
                             console.log("amount: "+amount+"\ntip: "+tip+"\nservice fee: "+serviceFee+"\ntax: "+tax+"\ntotal: "+Number(amount+tip+serviceFee+tax));//Logging
+                            let subtotal=amount;
                             //Use Braintree gateway to complete the transaction
                             gateway.transaction.sale({
                                 amount:Number(amount+tip+serviceFee+tax), //Calculate final cost
@@ -232,13 +191,40 @@ exports.handler=function(event,context,callback){
                                 }
                             }).then(function(result){
                                 if(result.success||result.transaction){//SUCCESS
-                                    let response={
-                                        "statusCode":200,
-                                        "headers":{},
-                                        "body":JSON.stringify(result),
-                                        "isBase64Encoded":"false"
-                                    };
-                                    callback(null,response);
+                                    //TODO: submit order to server
+                                    let orderConnector=new OrderConnector();
+
+                                    //id (any #?)
+                                    //cart (what format)
+                                    //recipient (username?)
+                                    //paymentInfo (cash, card)
+                                    //address (what format)
+                                    //driver (null?)
+                                    //subtotal (done)
+                                    //tax (done)
+                                    //total (amount)
+                                    //status (?)
+                                    let total=amount;
+                                    let dbResult=orderConnector.createOrder(id,cart,recipient,paymentInfo,address,driver,subtotal,tax,total,status);
+                                    if(dbResult){
+                                        let response={
+                                            "statusCode":200,
+                                            "headers":{},
+                                            "body":JSON.stringify(result),
+                                            "isBase64Encoded":"false"
+                                        };
+                                        callback(null,response);
+                                    }else{
+                                        //db err
+                                        console.log("INTO CREATE ORDER ERROR");
+                                        let response={
+                                            "statusCode":500,
+                                            "headers":{},
+                                            "body":JSON.stringify(dbResult),
+                                            "isBase64Encoded":"false"
+                                        };
+                                        callback(null,response);
+                                    }
                                 }else{//ERROR
                                     transactionErrors=result.errors.deepErrors();
                                     console.log("INTO CHECKOUT ERROR");
@@ -270,9 +256,85 @@ exports.handler=function(event,context,callback){
                             };
                             callback(null,response);
                         }
-
-
                     });
+                }
+            }else if(command.localeCompare("checkout2")===0){//API CALL POST to create cash order
+                //Declare variables (initialized to undefined)
+                let tip,cart,amount,street,city,state,zip;
+                let serviceFee=1; //Declare serviceFee variable
+
+                console.log("EVENT BODY: "+JSON.stringify(event.body));
+                try{
+                    event.body=JSON.parse(event.body);
+                }
+                catch(e){
+                    console.log("caught!");
+                    //Do nothing, running in test mode
+                }
+
+                if(event.body!=null&&event.body!==undefined){
+                    cart=event.body.cart;
+                    if(cart===undefined){
+                        console.log("INTO NO CART ERROR");
+                        let response={
+                            "statusCode":400,
+                            "headers":{},
+                            "body":event.body,
+                            "isBase64Encoded":"false"
+                        };
+                        callback(null,response);
+                    }
+                    //Get value of tip in float
+                    if(event.body.tip==parseFloat(event.body.tip,10)&&event.body.tip!=null&&event.body.tip!==undefined) tip=Math.abs(event.body.tip);
+                    else tip=0;
+
+                    //Check for int & absolute value of tip
+                    console.log(parseFloat(event.body.tip,10));
+
+                    let paymentConnector=new PaymentConnector();
+                    paymentConnector.getCartCost(cart).then(function(data){//Calculate cost of cart
+                        amount=Number(data);//Amount
+
+                        if(amount!==undefined){
+                            let tax=Number(Number(amount*0.06).toFixed(2)); //Calculate tax
+                            console.log("amount: "+amount+"\ntip: "+tip+"\nservice fee: "+serviceFee+"\ntax: "+tax+"\ntotal: "+Number(amount+tip+serviceFee+tax));//Logging
+                            let subtotal=amount;
+                            //TODO: submit order to server
+                            let orderConnector=new OrderConnector();
+
+                            //id (any #?)
+                            //cart (what format)
+                            //recipient (username?)
+                            //paymentInfo (cash, card)
+                            //address (what format)
+                            //driver (null?)
+                            //subtotal (done)
+                            //tax (done)
+                            //total (done)
+                            //status (?)
+                            let total=amount;
+                            let dbResult=orderConnector.createOrder(id,cart,recipient,paymentInfo,address,driver,subtotal,tax,total,status);
+                            if(dbResult){
+                                let response={
+                                    "statusCode":200,
+                                    "headers":{},
+                                    "body":JSON.stringify(result),
+                                    "isBase64Encoded":"false"
+                                };
+                                callback(null,response);
+                            }else{
+                                //db err
+                                console.log("INTO CREATE ORDER ERROR");
+                                let response={
+                                    "statusCode":500,
+                                    "headers":{},
+                                    "body":JSON.stringify(dbResult),
+                                    "isBase64Encoded":"false"
+                                };
+                                callback(null,response);
+                            }
+                        }
+                    }
                 }
             }else if(command.localeCompare("client")===0){
                 //Testing client functionality for transaction
