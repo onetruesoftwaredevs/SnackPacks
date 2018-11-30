13423f362270e0d6f6b38666ba1e32bdbde08240
Stephen <davi1138@purdue.edu>
2018-11-29 21:47:44 -0500

    Implemented local saving for users

--- rn_snack_pack_app/src/function/User.js
+++ rn_snack_pack_app/src/function/User.js
@@ -6,27 +6,59 @@
  *
  */
 
+import {Alert, AsyncStorage} from "react-native"
+
 class User {
-    constructor(name, id) {
+    constructor(name, custom_snackpacks) {
         this._name = name;
-        this._id = id;
         this._orders = [];
+        this._custom_snackpacks = custom_snackpacks;
     }
 
-    static setInstance(name, id) {
-        User.instance = new User(name, id);
+    static setInstance(name, custom_snackpacks) {
+        User.instance = new User(name, custom_snackpacks);
     }
 
     static getInstance() {
         return User.instance;
     }
 
+    getUserLocalDataJSON() {
+        return JSON.stringify({
+            name: this._name,
+            custom_snackpacks: this._custom_snackpacks
+        });
+    }
+
+    static loadLocal() {
+        let data = AsyncStorage.getItem("USER", null);
+        if (data !== null) {
+            return data;
+        }
+        return null;
+    }
+/*
+    static saveLocal() {
+        AsyncStorage.setItem("USER", this.getInstance().getUserLocalDataJSON(), (error) => {
+            Alert.alert('', error);
+        });
+    }*/
+
+    saveLocal = async () => {
+        try {
+            await AsyncStorage.setItem('USER', User.instance.getUserLocalDataJSON());
+        } catch (error) {
+            // Error saving data
+            Alert.alert('', error);
+        }
+    };
+
     loadOrders(orders) {
         this._orders = [];
         for (let i = 0; i < orders.length; i++) {
             let order = orders[i];
             if (order !== null) {
-                if (Number(order._recipient) === Number(this._id)) {
+                if (Number(order._recipient) === Number(this._name)) {
                     this._orders.push(order);
                 }
             }
@@ -39,10 +71,6 @@ class User {
 
     getName() {
         return this._name;
-    }
-
-    getId() {
-        return this._id;
     }
 
     getOrders() {
@@ -60,6 +88,60 @@ class User {
         this._orders.splice(0, 1);
     }
 
+    createCustomSnackPack(name) {
+        let custom_snackpack = {
+            name: name,
+            snacks: [],
+        };
+
+        if (!this._custom_snackpacks) {
+            this._custom_snackpacks = []
+        }
+
+        this._custom_snackpacks.push(custom_snackpack);
+        this.saveLocal();
+    }
+
+    updateCustomSnackPack(name, snacks) {
+        for (let i = 0; i < this._custom_snackpacks.length; i++) {
+            let csp = this._custom_snackpacks[i];
+            if (csp.name === name) {
+                csp.snacks = snacks;
+            }
+        }
+        this.saveLocal();
+    }
+
+    getCustomSnackPacks() {
+        if (this._custom_snackpacks === undefined) {
+            return [];
+        }
+        return this._custom_snackpacks;
+    }
+
+    getCustomSnackPack(name) {
+        for (let i = 0; i < this._custom_snackpacks.length; i++) {
+            let csp = this._custom_snackpacks[i];
+            if (csp.name === name) {
+                return csp;
+            }
+        }
+    }
+
+    getCustomSnackPackPrice(name) {
+        for (let i = 0; i < this._custom_snackpacks.length; i++) {
+            let csp = this._custom_snackpacks[i];
+            if (csp.name === name) {
+                let price = 0.0;
+                for (let j = 0; j < csp.snacks.length; j++) {
+                    let snack = csp.snacks[j];
+                    price += snack._price;
+                }
+                return price;
+            }
+        }
+
+    }
 }
 
 module.exports = User;
